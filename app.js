b35a8b9d7c317f812dfa906b5fb055e78d82fc34
“Yasith <48174322+yasithKumara@users.noreply.github.com>
2022-02-28 09:26:05 +0530

    fifth

--- app.js
+++ app.js
@@ -1,76 +1,80 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const mysql = require('mysql');
-const cors = require('cors');
+const mysql = require("mysql");
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{ 
-    res.send('<h1>Hello world</h1>')
-    }) 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
 
+const db = mysql.createConnection({
+  user: "user",
+  // host:"localhost", //database-kai-demo.cxkzqs74adyy.ap-southeast-1.rds.amazonaws.com
+  host: "database-kai-demo.cxkzqs74adyy.ap-southeast-1.rds.amazonaws.com",
+  password: "password",
+  // database:"usernode"
+  database: "user",
+});
 
-    const db = mysql.createConnection({
-        user:"user",
-        // host:"localhost", //database-kai-demo.cxkzqs74adyy.ap-southeast-1.rds.amazonaws.com
-        host:"database-kai-demo.cxkzqs74adyy.ap-southeast-1.rds.amazonaws.com",
-        password:"password",
-        // database:"usernode"   
-        database:"user"   
-    });
-    
-    app.get('/all',(req,res)=>{
-        //db.query("SELECT * FROM users",
-        db.query("SELECT * FROM user",
-        (err,result) =>{
-            if(err){
-                console.log(err);
-            }else{
-                res.send(result)
-            }
-    
-        })
-    })
-    
-    app.post('/create',(req,res)=>{
-        const name = req.body.name;
-        const password = req.body.password;
-        const imageurl = req.body.imageurl;
-    
-        console.log(name);
-    
-        //db.query("INSERT INTO users (name,password) VALUES (?,?)",[name,password],
-        // db.query("INSERT INTO user (name,password) VALUES (?,?)",[name,password],
-        db.query("INSERT INTO user (name,password,imageurl) VALUES (?,?)",[name,password,imageurl],
-        (err,result) =>{
-            if(err){
-                console.log(err);
-            }else{
-                res.send("result")
-            }
-    
-        })
-    })
-    
-    app.post('/login',(req,res)=>{
-        const name = req.body.name;
-        const password = req.body.password;
-    
-        db.query("SELECT * from user where name = ? AND password = ?",[name,password],
-        (err,result) =>{
-            if(err){
-                res.send({err: err});
-            }
-                if(result.length > 0){
-                    res.send(result);
-                }else{
-                    res.send({message: "wrong username or password"});
-                }   
-            
-        })
-    })    
+app.get("/all", (req, res) => {
+  //db.query("SELECT * FROM users",
+  db.query("SELECT * FROM user", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
 
-app.listen(process.env.PORT || 5000) //change to 5000
-module.exports = app
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+  const imageurl = req.body.imageurl;
+
+  console.log(name);
+
+  //db.query("INSERT INTO users (name,password) VALUES (?,?)",[name,password],
+  // db.query("INSERT INTO user (name,password) VALUES (?,?)",[name,password],
+//   db.query(
+//     "INSERT INTO user (name,password) VALUES (?,?)",
+//     [name, password],
+db.query(
+    "INSERT INTO user (name,password,imageurl) VALUES (?,?,?)",
+    [name, password, imageurl],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("result");
+      }
+    }
+  );
+});
+
+app.post("/login", (req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+
+  db.query(
+    "SELECT * from user where name = ? AND password = ?",
+    [name, password],
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send({ message: "wrong username or password" });
+      }
+    }
+  );
+});
+
+app.listen(process.env.PORT || 5000); //change to 5000
+module.exports = app;
